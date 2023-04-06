90ac0adcb7005aa09eb1acfcdc1f0b6e27a3f384
melachan23 <melissa.a.chan@accenture.com>
2023-04-06 17:15:32 +0000

    Added network module

--- main.tf
+++ main.tf
@@ -98,27 +98,6 @@ resource "azurerm_resource_group" "test" {
   location = "West Europe"
 }
 
-module "network" {
-  source              = "Azure/network/azurerm"
-  resource_group_name = azurerm_resource_group.example.name
-  address_spaces      = ["10.0.0.0/16", "10.2.0.0/16"]
-  subnet_prefixes     = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
-  subnet_names        = ["subnet1", "subnet2", "subnet3"]
-
-  subnet_service_endpoints = {
-    "subnet1" : ["Microsoft.Sql"], 
-    "subnet2" : ["Microsoft.Sql"],
-    "subnet3" : ["Microsoft.Sql"]
-  }
-
-  tags = {
-    environment = "dev"
-    costcenter  = "it"
-  }
-
-  depends_on = [azurerm_resource_group.example]
-}
-
 resource "azurerm_network_interface_security_group_association" "catapp-nic-sg-ass" {
   network_interface_id      = azurerm_network_interface.catapp-nic.id
   network_security_group_id = azurerm_network_security_group.catapp-sg.id
