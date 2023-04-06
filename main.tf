031d1c1dd9d6344b47da8d43ad0d2617351589b5
melachan23 <melissa.a.chan@accenture.com>
2023-04-06 17:25:21 +0000

    Added network module

--- main.tf
+++ main.tf
@@ -18,7 +18,7 @@ provider "azurerm" {
 module "network" {
   source  = "app.terraform.io/melc-training/network/azurerm"
   version = "3.5.0"
-  resource_group_name = "test"
+  resource_group_name = azurerm_resource_group.myresourcegroup.name
   # insert required variables here
 }
 resource "azurerm_resource_group" "myresourcegroup" {
